94eb5e8eb58873539ed8cd13274ba914ae68238b
NdefoAfam <ndefoafam@gmail.com>
2022-12-08 10:24:57 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Entry point
  *
  * Return: Always (1)
- */                                                                                                                            
+ */                                         
 
 int main(void)
 {
