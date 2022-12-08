7ef84cc86b7b8b9b896699cc55f26bed1ca7251d
NdefoAfam <ndefoafam@gmail.com>
2022-12-08 08:12:14 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return : Always 0 (Succeed)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece off art,\n");
+	return (0);
+
+}
