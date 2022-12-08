b55bfd51106424ac5af1fe855cdfc0925e878fdd
NdefoAfam <ndefoafam@gmail.com>
2022-12-08 10:11:29 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/* main - prints exactly and that piece of art is useful                                                                                             
+/* main - prints exactly and that piece of art is useful
  *
- *Return: Always 0 (Success)  
- */                                                                                                                                       
+ * Return: prints exactly and that piece of art is useful
+ */                                                                                                                            
 
 int main(void)
 {
