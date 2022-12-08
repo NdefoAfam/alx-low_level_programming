4f24c658997bc4b20c8bc0816061faaf39b9ff98
NdefoAfam <ndefoafam@gmail.com>
2022-12-08 07:37:38 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return:Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
+}
 
