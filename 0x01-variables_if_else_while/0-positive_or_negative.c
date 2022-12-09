33034ccc27d85db8a781d82930b40f274ccf6e22
NdefoAfam <ndefoafam@gmail.com>
2022-12-09 04:09:34 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - print if the numbeer is positive, negative or 0
+ * main - print if the numbeer is positive, negative or zero
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
