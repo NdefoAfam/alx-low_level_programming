f911ea84a76ee01fe7d955c07fce6a599ff9cbf1
NdefoAfam <ndefoafam@gmail.com>
2022-12-10 07:29:34 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,20 +7,22 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if (i < j)
+			if (j > i)
 			{
-				putchar((i / 10) + 48);
-				putcher((i % 10) + 48);
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i * 100 + j != 9899)
 				{
 					putchar(',');
 					putchar(' ');
