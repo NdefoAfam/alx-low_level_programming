eaba79d63807b88ceeb2e1e1d8f37d5528b20aea
NdefoAfam <ndefoafam@gmail.com>
2022-12-10 03:08:42 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of single-digit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		putchar('\n');
+		return (0);
+	}
