45f0713d89a549bc9b752a11d6122a272822248a
NdefoAfam <ndefoafam@gmail.com>
2022-12-13 00:37:26 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *print_alphabet - print all alphabet in lowercase
+  */
+
+void print_alphabet(void)
+{
+	charletter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter)
+
+			_putchar('\n');
+}
