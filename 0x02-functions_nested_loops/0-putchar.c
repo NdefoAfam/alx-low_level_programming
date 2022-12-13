6f8196e056c6c8ddd418133024b3819a4b9e195d
NdefoAfam <ndefoafam@gmail.com>
2022-12-13 14:21:27 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
-  * main - print _putchar
-  * is written by bwave
-  *Return: 0
-  */
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
