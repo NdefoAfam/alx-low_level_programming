432da0cc74cdc85afc8c536cb62b09ae53014858
NdefoAfam <ndefoafam@gmail.com>
2022-12-13 22:31:06 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-  *void print_alphabet_x10 - print 10 times the alphabet in lowercase
-  */
+ *void print_alphabet_x10 - print 10 times the alphabet in lowercase
+ */
 
 void print_alphabet_x10(void)
 {
