ddcde57d1fb8eb6d605c00decdb3894aeb243ed6
NdefoAfam <ndefoafam@gmail.com>
2022-12-14 16:51:55 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- *jack_bauer -  prints every minute of the day
+ *main - check the code
+ * jack_bauer -  prints every minute of the day
+ *Return: Always 0.
  */
+
 void jack_bauer(void)
 {
 	int hour, minute;
