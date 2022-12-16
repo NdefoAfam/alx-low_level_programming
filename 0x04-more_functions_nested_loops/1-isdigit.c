9e2da6a50ffc2fd3b7868cf90da818d67dfba455
NdefoAfam <ndefoafam@gmail.com>
2022-12-16 13:41:29 -0800

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isdigit - function that varies if a character is a digit or not
- * @c: tested character
- * Return: returs 1 if it is upper case, 0 if not
- */
+* _isdigit - function that verifies if a character is a digit or not
+* @c: tested character
+* Return: returs 1 if it is upper case, 0 if not
+*/
 
-int _isdigit.c(int c)
+int _isdigit(int c)
 {
 if ((c >= 48 && (c <= 57))
 return (1);
