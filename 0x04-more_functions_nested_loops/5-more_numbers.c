f4095aa71f23ca07d6864493c5274e939c79fc20
NdefoAfam <ndefoafam@gmail.com>
2022-12-16 15:42:57 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,14 +4,14 @@
 */
 void more_numbers(void)
 {
-int x, y;
+	int x, y;
 
-for (x = 0; x < 10; x++)
-{
-for (y = 0; y < 15; y++)
-
-{
-_putchar (y + '0');
-}
-_putchar('\n')
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			_putchar (y + '0');
+		}
+		_putchar('\n');
+	}
 }
