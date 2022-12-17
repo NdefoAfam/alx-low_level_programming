b2425a8a6ed5df1a86da939d4ac2a81d9485d21b
NdefoAfam <ndefoafam@gmail.com>
2022-12-16 16:01:31 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,16 +3,13 @@
 * print_line - prints a line
 *@n: integer params
 */
-
 void print_line(int n)
-
 {
 	int i;
 
 	if (n <= 0)
 		_putchar('\n');
-
-	for (i = 0; i <= n; i++);
+	for (i = 0; i <= n; i++)
 		_putchar('_');
 	_putchar('\n');
 }
