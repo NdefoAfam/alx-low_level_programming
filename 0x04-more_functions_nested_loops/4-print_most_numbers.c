a4c02a779a3979d58e716e4e3893d3b799994aaa
NdefoAfam <ndefoafam@gmail.com>
2022-12-16 16:30:59 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,13 @@
 /**
 *print_most_numbers - this prints most numbers
 */
-void print_most_numbers(void)
+void print_most_numbers(void);
 {
 	int x;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		if (x != 2 && x != 4)
-			_putchar(x + '0');
+		_putchar(x);
 	}
 	_putchar('\n');
 }
