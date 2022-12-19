13655622a73802b0d680ca7ff33e9d0f949c263d
NdefoAfam <ndefoafam@gmail.com>
2022-12-19 01:55:11 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98.c - function that takes a pointer to an int as parameter and updates the valueit points to 98
+ *
+ * @n: thos is the entry
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
