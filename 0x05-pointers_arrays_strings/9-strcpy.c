2e4af2126ffd62f6b1b9996e5d088f4794d92ee0
NdefoAfam <ndefoafam@gmail.com>
2022-12-19 21:45:41 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,17 @@
 char *_strcpy(char *dest, char *src)
 {
 
-	int l = 0;
-	int x = 0;
+int l = 0;
+int x = 0;
 
-	while (*(src + l) != '\0')
-	{
-		|++;
-	}
-	for ( ; x < l ; x++)
-	{
-		dest[x] = src[x];
-	}
+while (*(src + l) != '\0')
+{
+|++;
+}
+for ( ; x < l ; x++)
+{
+dest[x] = src[x];
+}
 	dest[l] = '\0';
 	return (dest);
 }
