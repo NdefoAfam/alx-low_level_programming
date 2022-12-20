32c0f2bf787dc62aeca78f3823092b6a0fb1ca3a
NdefoAfam <ndefoafam@gmail.com>
2022-12-19 19:34:36 -0800

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void -reset_to_98.c(int * n);
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -11,7 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void rev_string (char *s)
-int _putchar(char c);
 
 #endif
