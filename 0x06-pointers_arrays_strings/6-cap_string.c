96a83c5f4ea3f86459eefff60ccc638744e9f0a3
NdefoAfam <ndefoafam@gmail.com>
2022-12-21 15:10:18 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 	int index = 0;
 
 	while (str[index])
-	}
+	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
@@ -29,7 +29,9 @@ char *cap_string(char *str)
 			str[index - 1] == '}' ||
 			index == 0)
 			str[index] -= 32;
-			index++;
-		}
+
+		index++;
+	}
+
 	return (str);
 }
