87e4ad65d328519ff4303d661fe00dc942768039
NdefoAfam <ndefoafam@gmail.com>
2022-12-21 14:57:50 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,10 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-	{
-	return (s1[i] - s2[i])
-	}
-	i++;
+		{
+			return (s1[i] - s2[i])
+		}
+		i++;
 	}
 	return (0);
 }
