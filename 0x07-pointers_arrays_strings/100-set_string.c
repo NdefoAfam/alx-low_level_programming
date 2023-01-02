5165846a8ab3fce8831008fb20271bc18a3a22b2
NdefoAfam <ndefoafam@gmail.com>
2023-01-02 14:49:08 -0800

    Even more pointers, arrays and strings task 0 to task 10

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+/**
+ * File: 100-set_string.c
+ * Auth: EASY
+ */
+
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: The pointer.
+ * @to: The char.
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
