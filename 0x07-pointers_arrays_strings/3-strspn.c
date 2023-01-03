ffa20fa82acfbed9b84d4f83c4d204ba4230f5e9
NdefoAfam <ndefoafam@gmail.com>
2023-01-02 23:17:16 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/** 
-  * _strspn - Gets the length of a prefix substring.
-  * @s: The string to be searched.
-  * @accept: The prefix to be measured.
-  * 
-  * Return: The number of bytes in s which
-  * consist only of bytes from accept.
-  */
+/**
+ * _strspn - Entry point
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured. 
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
+ */
 
 unsigned int _strspn(char *s, char *accept)
+
 {
 	unsigned int bytes = 0;
 	int index;
@@ -34,5 +34,4 @@ unsigned int _strspn(char *s, char *accept)
 	}
 
 	return (bytes);
-
 }
