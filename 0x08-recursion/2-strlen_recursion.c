bc08837d58df6c1e5bb8bb0492e141a7f95c6b9f
NdefoAfam <ndefoafam@gmail.com>
2023-01-05 11:29:38 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int longit = 0;
+	int len = 0;
 
 	if (*s)
 	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
+		len++;
+		len += _strlen_recursion(s + 1);
 	{
-	return (longit);
+
+		return (len);
 }
