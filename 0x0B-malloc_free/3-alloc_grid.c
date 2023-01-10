1d47258eeba1f5fd80b4029318f9a3e926d2b32b
NdefoAfam <ndefoafam@gmail.com>
2023-01-10 12:02:11 -0800

    tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,48 +17,30 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (height <= 0 || width <= 0)
-
 		return (NULL);
 
 	array = (int **) malloc(sizeof(int *) * height);
 
 	if (array == NULL)
-
 		return (NULL);
-
 	for (i = 0; i < height; i++)
-
 	{
 		array[i] = (int *) malloc(sizeof(int) * width);
-
 		if (array[i] == NULL)
-
 		{
 			free(array);
-
-                        for (j = 0; j <= i; j++)
-
+			for (j = 0; j <= i; j++)
 				free(array[j]);
-
 			return (NULL);
-
 		}
-
 	}
 
 	for (i = 0; i < height; i++)
-
 	{
 		for (j = 0; j < width; j++)
-
 		{
-
 			array[i][j] = 0;
-
 		}
-
 }
-
 return (array);
-
 }
